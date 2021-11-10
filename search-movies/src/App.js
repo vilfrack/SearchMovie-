4896b7b2b86320eac78f15eaabc7f4b55bcc85d0
vilfrack <vilfrack21@gmail.com>
2021-11-10 16:05:42 -0300

    add search component

--- search-movies/src/App.js
+++ search-movies/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import 'bulma/css/bulma.css'
 
 import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Title>Search Moves</Title>
+        <Title>Find Movies</Title>
+        <div className='SearchForm-wrapper'>
+          <SearchForm />
+        </div>
+        
       </div>
     );
   }
