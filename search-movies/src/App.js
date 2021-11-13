6414f884f8c913562922e6f6e1b716793c1e0e06
vilfrack <vilfrack21@gmail.com>
2021-11-13 15:44:01 -0300

    se validan resultados

--- search-movies/src/App.js
+++ search-movies/src/App.js
@@ -7,12 +7,15 @@ import { SearchForm } from './components/SearchForm'
 import { MoviesList } from './components/MoviesList';
 
 class App extends Component {
-  state = { results: [] }
+  state = { usedSearch:false, results: [] }
 
   _handleResults = (results) => {
-    this.setState({ results })
+    this.setState({ results,usedSearch:true })
   }
 
+  _renderResults () {
+    return this.state.results.length === 0 ? <p>Sorry!!</p> : <MoviesList movies = {this.state.results}/> 
+  }
 
   render() {
     return (
@@ -21,7 +24,10 @@ class App extends Component {
         <div className='SearchForm-wrapper'>
           <SearchForm onResults={this._handleResults}/>
         </div>
-        {this.state.results.length === 0 ? <p>Sin resultados</p> : <MoviesList movies = {this.state.results}/> }
+
+        {this.state.usedSearch ? this._renderResults() : <small>Use the form to search a movie</small>}
+
+        
       </div>
     );
   }
